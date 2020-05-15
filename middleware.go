6df8d3d5dd5e29eef2e279cae8a487217556f5a1
Jay <jgodara@sarvika.com>
2020-05-15 12:54:42 +0530

    Corrected middleware definition

--- middleware.go
+++ middleware.go
@@ -7,21 +7,21 @@ import (
 	"golang.org/x/time/rate"
 )
 
-type Middleware func(http.Handler) http.HandlerFunc
+type Middleware func(http.Handler) http.Handler
 
-func loggingMiddleware(next http.Handler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		next.ServeHTTP(w, r)
 		logger.Infof("Request completed in %d ms", time.Since(start).Microseconds())
-	}
+	})
 }
 
 func rateLimiter(limit int) Middleware {
 
-	return func(next http.Handler) http.HandlerFunc {
+	return func(next http.Handler) http.Handler {
 		limiter := rate.NewLimiter(1, 200)
-		return func(w http.ResponseWriter, r *http.Request) {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if !limiter.Allow() {
 				w.WriteHeader(http.StatusTooManyRequests)
 
@@ -33,11 +33,11 @@ func rateLimiter(limit int) Middleware {
 			}
 
 			next.ServeHTTP(w, r)
-		}
+		})
 	}
 }
 
-func combineHandlers(root http.HandlerFunc, mwf ...Middleware) (handler http.HandlerFunc) {
+func combineHandlers(root http.Handler, mwf ...Middleware) (handler http.Handler) {
 	handler = root
 	for _, m := range mwf {
 		handler = m(handler)
